882dc8d2c42f5ea4d88675254bbe1f52aa0f56bd
91950 <rsd241101@gmail.com>
2022-10-10 20:24:26 +0530

    my first commit

---
+++ SampleProj/src/LargestofThree.java
@@ -0,0 +1,18 @@
+
+public class LargestofThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 15;
+		int num2 = 11;
+		int num3 = 20;
+		if(num1 > num2 && num1 > num3) {
+			System.out.println(num1+" is largest number");
+			}else if(num2 > num3 && num2 > num1) {
+				System.out.println(num2+ "is largest number");
+			}else 
+				System.out.println(num3+ " is largest number");
+
+	}
+
+}
