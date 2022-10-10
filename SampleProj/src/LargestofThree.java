df186226fc85a61943ef882e32978aae2e6f6910
91950 <rsd241101@gmail.com>
2022-10-10 21:12:49 +0530

    third coomit

--- SampleProj/src/LargestofThree.java
+++ SampleProj/src/LargestofThree.java
@@ -5,7 +5,7 @@ public class LargestofThree {
 		// TODO Auto-generated method stub
 		int num1 = 14;
 		int num2 = 12;
-		int num3 = 20;
+		int num3 = 30;
 		if(num1 > num2 && num1 > num3) {
 			System.out.println(num1+" is largest number");
 			}else if(num2 > num3 && num2 > num1) {
